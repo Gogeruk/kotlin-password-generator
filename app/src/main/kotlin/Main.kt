51f2d09983dcd0a3e0dc1c5ac4af8ea6ce1af628
Mykyta <korsikov_php@upiple.com>
2024-11-06 11:40:24 +0200

    [APP] hello!

---
+++ app/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+
+    val test = 111111111
+
+
+    println(test)
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+    println("Hello from Main!")
+}
